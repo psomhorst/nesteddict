7a97ff15aa553943b4b216b21fbeb15483754007
Peter Somhorst <peter.somhorst@gmail.com>
2016-06-05 22:28:01 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='nesteddict',
-      version='0.1.0',
+      version='0.1.1',
       description='This module provides a nested-aware dictionary.',
       author='Peter Somhorst',
       author_email='peter.somhorst@gmail.com',
